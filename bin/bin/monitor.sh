aabc70c5b2d8788f8b6c56eacb4f1e75d58227bc
fraso-dev <fraso.dev@protonmail.com>
2023-03-19 14:26:23 +0100

    feat: monitor layout script

--- bin/bin/monitor.sh
+++ bin/bin/monitor.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-PRIMARY_OUTPUT=`xrandr | grep -w connected | cut -d ' ' -f 1`
-RATE144=144
-RATE60=60
-WIDE4K=3440x1440
-FULLHD=1920x1080
-DP='DP-0'
-HDMI='HDMI-0'
+DIR="${HOME}/.screenlayout/default"
+FILENAME=$(basename $DIR)
 
-if [[ $PRIMARY_OUTPUT == $DP ]]; then
-  xrandr --output $DP --mode $WIDE4K --rate $RATE144 --primary
-	notify-send --urgency=normal -t 3000 "$PRIMARY_OUTPUT - ${RATE}hz"
-elif [[ $PRIMARY_OUTPUT == $HDMI ]]; then
-    xrandr --output $HDMI --mode $FULLHD --rate $RATE60 --primary
+if [[ -f "${DIR}" ]]; then
+    notify-send --urgency=normal -t 3000 "Setting screenlayout to $FILENAME"
+    if [[ -x $DIR ]]; then
+        echo "changin $FILENAME permissions"
+        chmod +x $DIR
+    fi
+
+    "${DIR}"
 else 
-  exit 1;
+	notify-send --urgency=normal -t 3000 "screenlayout file '${DIR}' not found"
+    ${HOME}/.dotfiles/rofi/.config/rofi/scripts/monitor.sh
 fi
