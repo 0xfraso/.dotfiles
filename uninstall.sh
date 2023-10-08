d7753653bdf8e199b7df7cc135c3618448c79e3f
fraso-dev <fraso.dev@protonmail.com>
2023-10-08 17:34:19 +0200

    cleanup

--- uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
-do 
-  echo "unstow $folder"
-  stow -D $folder
-done
-
--- wayland/.config/hypr/scripts/toggle_hyprland_blur.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ "$(hyprctl getoption decoration:blur_size | grep int | cut -d ' ' -f2)" == 0 ]]; then
-    hyprctl keyword decoration:blur_size 10
-else 
-    hyprctl keyword decoration:blur_size 0
-fi
