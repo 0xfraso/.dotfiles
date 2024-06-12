15700b50b34c5781c1081d1d334f210ea8795b3d
0xfraso <fraso.dev@protonmail.com>
2024-06-12 08:52:16 +0200

    nvim: minimal lualine config

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -31,8 +31,7 @@ return {
       sections = {
         lualine_a = { "branch" },
         lualine_b = {
-          LazyVim.lualine.root_dir(),
-          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          LazyVim.lualine.root_dir({ icon = "", color = LazyVim.ui.fg("Green") }),
           {
             "filename",
             symbols = {
@@ -51,49 +50,10 @@ return {
             cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
             color = function() return LazyVim.ui.fg("Statement") end,
           },
-          -- stylua: ignore
-          {
-            function() return require("noice").api.status.mode.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-            color = function() return LazyVim.ui.fg("Constant") end,
-          },
-          -- stylua: ignore
-          -- stylua: ignore
-          {
-            require("lazy.status").updates,
-            cond = require("lazy.status").has_updates,
-            color = function() return LazyVim.ui.fg("Special") end,
-          },
-          {
-            "diff",
-            symbols = {
-              added = icons.git.added,
-              modified = icons.git.modified,
-              removed = icons.git.removed,
-            },
-            source = function()
-              local gitsigns = vim.b.gitsigns_status_dict
-              if gitsigns then
-                return {
-                  added = gitsigns.added,
-                  modified = gitsigns.changed,
-                  removed = gitsigns.removed,
-                }
-              end
-            end,
-          },
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
         },
         lualine_y = {
-          {
-            "diagnostics",
-            symbols = {
-              error = icons.diagnostics.Error,
-              warn = icons.diagnostics.Warn,
-              info = icons.diagnostics.Info,
-              hint = icons.diagnostics.Hint,
-            },
-          },
-          "mode",
+          { 'mode', fmt = function(str) return str:sub(1,1) end }
         },
         lualine_z = {
           {
